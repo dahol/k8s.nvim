89b4b1d294aecbcde5bb1d3d91fb38bd5516f6e6
Daniel Meek Holter <daniel.holter@sikri.no>
2026-02-23 14:10:43 +0100

    Fix compatibility issue with older Neovim versions by using separate BufNewFile and BufRead events

--- lua/k8s/init.lua
+++ lua/k8s/init.lua
@@ -3,7 +3,8 @@ local M = {}
 -- Setup function for the k8s.nvim plugin
 function M.setup()
   -- Set up autocommands for YAML files to enable completion
-  vim.api.nvim_create_autocmd("BufNewFile,BufRead", {
+  -- Using separate events for better compatibility with older Neovim versions
+  vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
     pattern = "*.yaml",
     callback = function()
       if vim.api.nvim_buf_get_option(0, "buftype") == "" then
